e60fd7e622c23f2b482f300908152a75fb7237f8
nonchris <git@chris-ge.de>
2021-05-25 16:44:51 +0200

    Add extract_id_from_string() to easily extract a discord id from string

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,3 +1,6 @@
+import re
+from typing import Union
+
 import discord
 from discord.errors import Forbidden
 
@@ -61,3 +64,16 @@ def make_embed(title="", color=blue_light, name="‌", value="‌", footer=None)
         emb.set_footer(text=footer)
 
     return emb
+
+
+def extract_id_from_string(content: str) -> Union[int, None]:
+    """!
+    Scans string to extract user/guild/message id\n
+    Can extract IDs from mentions or plaintext
+
+    @return extracted id as int if exists, else None
+    """
+    # matching string that has 18 digits surrounded by non-digits or start/end of string
+    match = re.match(r'(\D+|^)(\d{18})(\D+|$)', content)
+
+    return int(match.group(2)) if match else None
