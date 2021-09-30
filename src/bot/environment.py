dd30eecb75cd6bb8c110bcc3a78481fcbf3c57c3
nonchris <git@chris-ge.de>
2021-09-30 21:14:30 +0200

    Support configuration using json
    json is now an alternative way to configure the bots variables like TOKEN or PREFIX.
    If a parameter is given using env-variable and json the one from env will be preferred.
    
    I is suggested to continue using env variables, but it's neat to have both, especially for beginners.
    The path to the file is hard-coded as ./data/config.json at the moment

--- src/bot/environment.py
+++ src/bot/environment.py
@@ -1,24 +1,56 @@
+import json
 import os
-import logging
+from .log_setup import logger
 
 ### @package environment
 #
 # Interactions with the environment variables.
 #
+from typing import Dict, Optional
 
-def load_env(key: str, default: str) -> str:
+
+def load_env(key: str, default: str, config_dict=None) -> str:
     """!
-    os.getenv() wrapper that handles the case of None-types for not-set env-variables\n
+    Function to load a key from environment or from a config-dict\n
+    Handles None-types for not set env-variables by returning the default.\n
+    Prefers env-variables with same name over contents read from config-file\n
     Does also replace specified expressions like {PREFIX} with actual content.
 
     @param key: name of env variable to load
     @param default: default value if variable couldn't be loaded
+    @param config_dict: json-like dict
     @return value of env variable or default value
     """
-    value = os.getenv(key)
-    if value:
+
+    env_value = os.getenv(key)  # get key from environment
+
+    # try to get key also from config file
+    conf_val = None
+    if isinstance(config_dict, dict):
+        conf_val = config_dict.get(key, None)
+
+    # Decide which value to take
+    if env_value and conf_val:
+        logger.info(f"Gained '{key}' from environment and config - preferring env-variable")
+        value = env_value
+    elif env_value:
+        logger.debug(f"Gained '{key}' from environment only, using this value")
+        value = env_value
+    elif conf_val:
+        logger.debug(f"Gained '{key}' from config-file only, using this value")
+        value = conf_val
+    else:
+        value = None
+
+    # catch token and prefix value, since it doesn't need the extra replace handling below
+    # also PREFIX isn't defined yet...
+    if key == "TOKEN" or key == "PREFIX":
+        return value
+
+    if value is not None:
         try:
             return value.replace("{PREFIX}", PREFIX)
+        # this happens when a variable is loaded before PREFIX
         except NameError as e:
             logger.error(
                 f"Can't replace expressions for: '{key}' {e.__repr__()}.\n"
@@ -30,13 +62,28 @@ def load_env(key: str, default: str) -> str:
     return default
 
 
-logger = logging.getLogger('my-bot')
+def load_conf_file(config_file='./data/config.json') -> Optional[Dict[str, str]]:
+    if os.path.isfile(config_file):
+        logger.debug(f"Config file '{config_file}' exists, trying to read")
+        try:
+            with open(config_file, "r") as jsonfile:
+                return json.load(jsonfile)
 
-TOKEN = os.getenv("TOKEN")  # reading in the token from environment - there is no default...
+        except OSError:
+            logger.warning(f"Can't open or read config file: '{config_file}'")
+
+    else:
+        logger.debug(f"No config-file was found under '{config_file}', trying to continue")
+
+
+cfg_dict = load_conf_file('./data/config.json')
+
+TOKEN = load_env("TOKEN", '', config_dict=cfg_dict)  # reading in the token from environment - there is no default...
 
 # loading optional env variables
-PREFIX = load_env("PREFIX", "b!")
-VERSION = load_env("VERSION", "unknown")  # version of the bot
-OWNER_NAME = load_env("OWNER_NAME", "unknown")   # owner name with tag e.g. pi#3141
-OWNER_ID = int(load_env("OWNER_ID", "100000000000000000"))  # discord id of the owner
-ACTIVITY_NAME = load_env("ACTIVITY_NAME", f"{PREFIX}help")  # activity bot plays
+PREFIX = load_env("PREFIX", "b!", config_dict=cfg_dict)
+VERSION = load_env("VERSION", "unknown", config_dict=cfg_dict)  # version of the bot
+OWNER_NAME = load_env("OWNER_NAME", "unknown", config_dict=cfg_dict)   # owner name with tag e.g. pi#3141
+OWNER_ID = int(load_env("OWNER_ID", "100000000000000000", config_dict=cfg_dict))  # discord id of the owner
+ACTIVITY_NAME = load_env("ACTIVITY_NAME", f"{PREFIX}help", config_dict=cfg_dict)  # activity bot plays
+
