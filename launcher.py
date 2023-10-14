7088d7f01e5cfcef1b328e25aad982ffb9c09aa3
nonchris <git@chris-ge.de>
2023-10-14 22:12:22 +0200

    make package executable without launcher script

--- launcher.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""
-A boot-script to start the bot without installing as package
-"""
-if __name__ == '__main__':
-    from src.discord_bot import start_bot
-    start_bot()
--- src/discord_bot/__init__.py
+++ src/discord_bot/__init__.py
@@ -1,5 +1,13 @@
-from .main import start_bot
-from .version import VERSION
+import sys
+
+if __package__ is None and not hasattr(sys, "frozen"):
+    import os.path
+
+    path = os.path.realpath(os.path.abspath(__file__))
+    sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
+
+from discord_bot.main import start_bot
+from discord_bot.version import VERSION
 
 # this version will be read by setup.py and the bot itself
 __version__ = VERSION
@@ -7,3 +15,6 @@ __version__ = VERSION
 
 def main():
     start_bot()
+
+if __name__ == '__main__':
+    main()
